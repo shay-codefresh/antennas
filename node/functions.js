7d0b56e959a6c8ed4247e88e24d5565002b26379
shay ben asaf <shay@shays-MacBook-Pro.local>
2016-07-24 12:48:53 +0300

    all unitests

--- node/functions.js
+++ node/functions.js
@@ -4,8 +4,42 @@
 
 var _ = require('lodash-node');
 var fs = require("fs");
+var lodash_check = false;
+
+function getlodashcheck() {
+    return lodash_check;
+}
+
+function deletetest(name) {
+    fs.unlink(__dirname + '/'+name, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+function createfile(json,name, callback) {
+    fs.writeFile(__dirname + '/'+name, JSON.stringify(json, null, 2), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    callback(null)
+}
+
+/**
+ *
+ function createtest(json, callback) {
+    fs.writeFile(__dirname + '/test.json', JSON.stringify(json, null, 2), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+}
+ */
 
 function myread(filename, callback) {
+    lodash_check=false;
     fs.readFile(__dirname + '/' + filename, function (err, result) {
         if (err) {
 //check if the path is wrong
@@ -16,33 +50,51 @@ function myread(filename, callback) {
             var containers = JSON.parse(result);
             //console.log("Output Content : \n"+ array);
             var answer = [];
+            var amount = 0;
 //need to be checked for num containers
+
             for (var container in containers) {
-                answer.push(new Date(containers[container].created));
+                containers[container].created = new Date(containers[container].created);
+                answer.push(containers[container]);
+//                console.log(new Date(containers[container].created));
+//                console.log(answer[amount]);
+                amount++;
+
+
             }
 
+            //console.log(containers);
+            //process.env.USE_LODASH='undefined';
             if (!process.env.USE_LODASH) {
 //check the function if we have lodash env , check the function, check the varible
+//                console.log("heyyyyy")
+//                console.log(process.env.USE_LODASH)
                 answer.sort(function (date1, date2) {
-                    if (date1 > date2) return 1;
-                    if (date1 < date2) return -1;
+                    if (date1.created > date2.created) return 1;
+                    if (date1.created < date2.created) return -1;
                     return 0;
                 });
                 callback(null, answer);
+
             }
             else {
+                lodash_check = true;
 //check the function if we have lodash env , check the function, check the varible
                 answer = _.sortBy(answer, function (value) {
                     //    console.log(value.getTime());
-                    return value.getTime();
-
+                    return value.created.getTime();
+                    //console.log(value.created.getTime());
                 });
+
+                // console.log(answer);
                 callback(null, answer);
+
             }
         }
     });
-
-
 };
 
 module.exports.myread = myread;
+module.exports.createfile = createfile;
+module.exports.deletetest = deletetest;
+module.exports.getlodashcheck = getlodashcheck;
--- node/test/functions.unit.spec.js
+++ node/test/functions.unit.spec.js
@@ -27,73 +27,245 @@ describe("functions tests", function () {
                 });
             });
 
-                it("should return an array of containers with amount of 3", function (done) {
-                    tools.myread("/containers2.json", function (err, res) {
-                        if (err) {
-                            return done(err);
-                        }
-                        else {
-                            expect(res).to.be.instanceof(Array);
-                            expect(res.length).to.equal(3);
-                            done();
-                        }
-                    });
+            it("should check if the array of containers amount of 3 objects, creates and deletes file", function (done) {
+                var containers={
+                "container_1": {
+                    "id": "1",
+                        "name": "container 1",
+                        "cpu": 90,
+                        "mem": 60,
+                        "layers": [
+                        "1",
+                        "12",
+                        "123",
+                        "1234",
+                        "12345"
+                    ],
+                        "created": "Mon Jul 18 2016 12:33:00 GMT+0300 (IDT)"
+                },
+                "container_2": {
+                    "id": "2",
+                        "name": "container 2",
+                        "cpu": 70,
+                        "mem": 40,
+                        "layers": [
+                        "1",
+                        "12",
+                        "123",
+                        "1234",
+                        "12345"
+                    ],
+                        "created": "Mon Jul 18 2010 10:50:00 GMT+0300 (IDT)"
+                },
+                "container_3": {
+                    "id": "3",
+                        "name": "container 3",
+                        "cpu": 60,
+                        "mem": 30,
+                        "layers": [
+                        "1",
+                        "12",
+                        "123",
+                        "1234",
+                        "12345"
+                    ],
+                        "created": "Wed Jul 18 2012 10:00:00 GMT+0300 (IDT)"
+                }
+            };
+            var name="test.json";
+                tools.createfile(containers,name,function (error) {
+                    if(error){
+                        return console.log(error);
+                    }
+                    else {
+
+                        tools.myread(name, function (err, res) {
+                            if (err) {
+                                tools.deletetest(name);
+                                return done(err);
+                            }
+                            else {
+
+                                expect(res).to.be.instanceof(Array);
+                                expect(res.length).to.equal(3);
+                                tools.deletetest(name);
+                                done();
+                            }
+                        });
+
+                    }
+                });
+
             });
 
-            it("should return the 5 containers sorted by date ", function (done) {
+
+////*****
+            it("should return if the 3 containers sorted by date , creates and deletes file", function (done) {
+                var containers={
+                    "container_1": {
+                        "id": "1",
+                        "name": "container 1",
+                        "cpu": 90,
+                        "mem": 60,
+                        "layers": [
+                            "1",
+                            "12",
+                            "123",
+                            "1234",
+                            "12345"
+                        ],
+                        "created": "Mon Jul 18 2016 12:33:00 GMT+0300 (IDT)"
+                    },
+                    "container_2": {
+                        "id": "2",
+                        "name": "container 2",
+                        "cpu": 70,
+                        "mem": 40,
+                        "layers": [
+                            "1",
+                            "12",
+                            "123",
+                            "1234",
+                            "12345"
+                        ],
+                        "created": "Mon Jul 18 2010 10:50:00 GMT+0300 (IDT)"
+                    },
+                    "container_3": {
+                        "id": "3",
+                        "name": "container 3",
+                        "cpu": 60,
+                        "mem": 30,
+                        "layers": [
+                            "1",
+                            "12",
+                            "123",
+                            "1234",
+                            "12345"
+                        ],
+                        "created": "Wed Jul 18 2012 10:00:00 GMT+0300 (IDT)"
+                    }
+                };
+                var name='test.json'
+                tools.createfile(containers,name,function(error){
+                    if(error){
+                        return console.log(error)
+                    }
+                    else{
+
+
+                        tools.myread(name, function (err, res) {
+                            if (err) {
+                                tools.deletetest(name);
+                                return done(err);
+                            }
+                            else {
+                                tools.deletetest(name);
+                                for (var i = 0; i < res.length; i++) {
+                                    res[i].created = res[i].created.toString();
+                                }
+                                //console.log(res);\
+                                var sorted = [ {
+                                    "id": "2",
+                                    "name": "container 2",
+                                    "cpu": 70,
+                                    "mem": 40,
+                                    "layers": ["1", "12", "123", "1234", "12345"],
+                                    "created": "Sun Jul 18 2010 10:50:00 GMT+0300 (IDT)"
+                                },{
+                                    "id": "3",
+                                    "name": "container 3",
+                                    "cpu": 60,
+                                    "mem": 30,
+                                    "layers": ["1", "12", "123", "1234", "12345"],
+                                    "created": "Wed Jul 18 2012 10:00:00 GMT+0300 (IDT)"
+                                }, {
+                                    "id": "1",
+                                    "name": "container 1",
+                                    "cpu": 90,
+                                    "mem": 60,
+                                    "layers": ["1", "12", "123", "1234", "12345"],
+                                    "created": "Mon Jul 18 2016 12:33:00 GMT+0300 (IDT)"
+                                }]
+                                for (var i = 0; i < res.length; i++) {
+                                    expect(res[i]).to.deep.equal(sorted[i]);
+
+                                }
+                                done();
+                            }
+                        });
+
+
+                    }
+                });
+            });
+///
+
+
+            it("should return if the 5 containers sorted by date ", function (done) {
                 tools.myread("/containers.json", function (err, res) {
                     if (err) {
                         return done(err);
                     }
                     else {
-                        console.log(res);
-                        expect(res.toString()).to.equal(
-                            "Tue Sep 13 1994 11:33:00 GMT+0200 (IST)," +
-                            "Mon Jul 18 2016 10:00:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 10:33:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 10:50:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 12:33:00 GMT+0300 (IDT)"
-                        );
-                        done();
-                    }
-                });
-            });
+                        for (var i = 0; i < res.length; i++) {
+                            res[i].created = res[i].created.toString();
+                        }
+                        //console.log(res);\
+                        var sorted = [{
+                            "id": "2",
+                            "name": "container 2",
+                            "cpu": 80,
+                            "mem": 50,
+                            "layers": ["1", "12", "123", "1234", "12345"],
+                            "created": "Tue Sep 13 1994 11:33:00 GMT+0200 (IST)"
+                        }, {
+                            "id": "4",
+                            "name": "container 4",
+                            "cpu": 60,
+                            "mem": 30,
+                            "layers": ["1", "12", "123", "1234", "12345"],
+                            "created": "Mon Jul 18 2016 10:00:00 GMT+0300 (IDT)"
+                        }, {
+                            "id": "5",
+                            "name": "container 5",
+                            "cpu": 50,
+                            "mem": 20,
+                            "layers": ["1", "12", "123", "1234", "12345"],
+                            "created": "Mon Jul 18 2016 10:33:00 GMT+0300 (IDT)"
+                        }, {
+                            "id": "3",
+                            "name": "container 3",
+                            "cpu": 70,
+                            "mem": 40,
+                            "layers": ["1", "12", "123", "1234", "12345"],
+                            "created": "Mon Jul 18 2016 10:50:00 GMT+0300 (IDT)"
+                        }, {
+                            "id": "1",
+                            "name": "container 1",
+                            "cpu": 90,
+                            "mem": 60,
+                            "layers": ["1", "12", "123", "1234", "12345"],
+                            "created": "Mon Jul 18 2016 12:33:00 GMT+0300 (IDT)"
+                        }]
+                        for (var i = 0; i < res.length; i++) {
+                            expect(res[i]).to.deep.equal(sorted[i]);
 
-
-
-
-            it("should return the 3 containers sorted by date", function (done) {
-                tools.myread("/containers2.json", function (err, res) {
-                    if (err) {
-                        return done(err);
-                    }
-                    else {
-                        console.log(res);
-                        expect(res.toString()).to.equal(
-                            "Mon Jul 18 2016 10:00:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 10:50:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 12:33:00 GMT+0300 (IDT)"
-                        );
+                        }
                         done();
                     }
                 });
             });
 //
-            it("should return an array of containers sorted (using lodash) by date if the environment variable USE_LODASH is exsits", function(done){
-                tools.myread("/containers.json", function(err, res){
+            it("should return an array of containers sorted (using lodash) by date if the environment variable USE_LODASH is exsits", function (done) {
+                process.env.USE_LODASH="HRHWHW";
+                tools.myread("/containers.json", function (err, res) {
                     if (err) {
                         return done(err);
                     }
                     else {
-                        expect(process.env.USE_LODASH).to.not.equal(null);
-                        console.log(res);
-                        expect(res.toString()).to.equal(
-                            "Tue Sep 13 1994 11:33:00 GMT+0200 (IST)," +
-                            "Mon Jul 18 2016 10:00:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 10:33:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 10:50:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 12:33:00 GMT+0300 (IDT)"
-                        );
+                        expect(process.env.USE_LODASH).to.not.equal(undefined);
+                        expect(tools.getlodashcheck()).to.equal(true);
+//                        console.log(process.env.USE_LODASH);
                         done();
                     }
                 });
@@ -115,28 +287,20 @@ describe("functions tests", function () {
                 });
             });
 
-            it("should return an array of containers sorted by date without using lodash if the environment variable USE_LODASH is not exsits", function(done){
-                tools.myread("/containers.json", function(err, res){
+            it("should return an array of containers sorted by date without using lodash if the environment variable USE_LODASH is not exsits", function (done) {
+                process.env.USE_LODASH='';
+                tools.myread("/containers.json", function (err, res) {
                     if (err) {
                         return done(err);
                     }
                     else {
-                        expect(process.env.USE_LODASH).to.equal(undefined);
-                        console.log(res);
-                        expect(res.toString()).to.equal(
-                            "Tue Sep 13 1994 11:33:00 GMT+0200 (IST)," +
-                            "Mon Jul 18 2016 10:00:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 10:33:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 10:50:00 GMT+0300 (IDT)," +
-                            "Mon Jul 18 2016 12:33:00 GMT+0300 (IDT)"
-                        );
+                        expect(process.env.USE_LODASH).to.equal('');
+                        expect(tools.getlodashcheck()).to.equal(false);
+//                        console.log(process.env.USE_LODASH);
                         done();
                     }
                 });
             });
-
-
-
 
         });
 
